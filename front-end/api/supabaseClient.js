366b5ba69b2fb4de47a404a92027f53df1e0318e
Everett Yan <everettzyan@gmail.com>
2025-05-05 20:22:09 -0500

    fixed all errors!

--- front-end/api/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://lcylwbtnglajjdvpxnsq.supabase.co'
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-export default supabase;
--- front-end/app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomeScreen() {
-    <View className='flex-1 justify-center items-center'>
-        <View>
-            <Text>Hello world</Text>
-        </View>
-    </View>
-};
